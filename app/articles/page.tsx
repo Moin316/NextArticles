53a67b076effe80a23b3049ffb508aa6f65568e8
Shaik Moin <moinshaik1603@gmail.com>
2025-05-31 20:15:42 +0530

    worked on Articles page

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -4,15 +4,15 @@ import ArticlesLoading from "@/components/articles/ArticlesLoading";
 import { Suspense } from "react";
 
 type PageProps = {
-    searchParams: {
-        search?: string;
-        page?: string;
-    };
+  searchParams: {
+    search?: string;
+    page?: string;
+  };
 };
 
-const page:React.FC<PageProps> = ({searchParams}) => {
+export default function Page({ searchParams }: PageProps) {
   const search = searchParams.search || "";
-  
+
   return (
     <div className="min-h-screen bg-gradient-to-r from-blue-50 to-purple-50 dark:from-gray-900 dark:to-gray-800">
       <main className="container mx-auto px-4 py-12 sm:px-6 lg:px-8">
@@ -23,11 +23,9 @@ const page:React.FC<PageProps> = ({searchParams}) => {
           <ArticleSarchInput />
         </div>
         <Suspense fallback={<ArticlesLoading />}>
-          <AllArticlesPage searchParams={search}/>
+          <AllArticlesPage searchParams={search} />
         </Suspense>
       </main>
     </div>
   );
-};
-
-export default page;
+}
